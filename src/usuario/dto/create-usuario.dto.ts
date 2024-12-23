6a83a764a938ff865738c87c1f4fb5ab6dc9d33d
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-23 18:49:40 -0300

    Adicionando regras do class-validator pipes

--- src/usuario/dto/create-usuario.dto.ts
+++ src/usuario/dto/create-usuario.dto.ts
@@ -1,6 +1,23 @@
+import { IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator';
+
 export class CreateUsuarioDto {
+    @IsString()
+    @MinLength(3)
+    @IsNotEmpty()
     nome: string;
+    
+    @IsOptional()
+    @IsString()
+    @MinLength(3)
     sobrenome?: string;
+    
+    @IsString()
+    @MinLength(10)
+    @IsNotEmpty()
     email: string;
+    
+    @IsString()
+    @MinLength(6, { message: "A senha deve conter no mínimo 6 caracteres."})
+    @IsNotEmpty()
     senha: string;
 }
--- src/usuario/dto/update-usuario.dto.ts
+++ src/usuario/dto/update-usuario.dto.ts
@@ -1,6 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUsuarioDto } from './create-usuario.dto';
+import { IsDate, IsOptional } from 'class-validator';
 
 export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+    @IsDate()
+    @IsOptional()
     dt_atualizacao: Date;
 }
