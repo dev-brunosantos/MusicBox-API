523b55cfb1a499b2864bc0b328343ccb1b33f687
Bruno Santos <brunosdesenvolvedor@gmail.com>
2024-12-26 11:38:58 -0300

    Disponibilizando o modulo do Prisma para o uso no modulo de Cargos

--- src/cargo/cargo.module.ts
+++ src/cargo/cargo.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CargoService } from './cargo.service';
 import { CargoController } from './cargo.controller';
+import { PrismaModule } from '../prisma/prisma.module'
 
 @Module({
+  imports: [PrismaModule],
   controllers: [CargoController],
   providers: [CargoService],
 })
